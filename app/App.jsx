3ee04b3daebfe1ada5f22b29f2a05cc8e500bb5d
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-08-28 19:00:56 -0500

    Assets for seed

--- app/App.jsx
+++ app/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function App() {
   return (
-    <div>it werks!</div>
+    <div>
+      it werks!
+    </div>
   );
-};
+}
--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 
 import App from './App';
 
+import './styles/app.scss';
+
 render(
   <App />,
   document.getElementById('app')
